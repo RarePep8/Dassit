4d15ce9dcb1b1633c4f4cd5ae5959e38e577e53b
rarepep8 <alex.wong.utsc@gmail.com>
2018-10-28 22:34:09 -0400

    Added projectile spawning, movement, and collision with ground

---
+++ src/projectile2.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var direction_scale = 1;
+func _ready():
+    # Called when the node is added to the scene for the first time.
+    # Initialization here
+    connect("body_enter", self, "contact")
+
+#func _process(delta):
+#    # Called every frame. Delta is time since last frame.
+#    # Update game logic here.
+#    pass
+
+func _physics_process(delta):
+    var collision = move_and_collide(direction_scale * Vector2(1,0))
+    if(collision != null and collision.get_collider().get_name() == "TileMap"):
+        print(collision.get_collider().get_name())
+        destroy()
+    
+func contact(body):
+    print("hi")
+    destroy()
+    
+func destroy():
+    queue_free()
--- src/sprites/tophat/tophat.gd
+++ src/sprites/tophat/tophat.gd
@@ -4,7 +4,7 @@ const GRAVITY = 400.0
 const WALK_SPEED = 200
 const JUMP_SPEED = 250
 var velocity = Vector2()
-
+    
 func _physics_process(delta):
     velocity.y += delta*GRAVITY
     if Input.is_action_pressed("ui_left"):
@@ -39,3 +39,13 @@ func _physics_process(delta):
     # The second parameter of move_and_slide is the normal pointing up.
     # In the case of a 2d platformer, in Godot upward is negative y, which translates to -1 as a normal.
     velocity = move_and_slide(velocity, Vector2(0, -1))
+
+
+    if Input.is_action_pressed("p1_shoot"):
+        var bullet = load("res://projectile2.tscn")
+        var bi = bullet.instance()
+        bi.position = get_position() + Vector2(-20 if $AnimatedSprite.flip_h else 20, 0)
+        bi.direction_scale = -1 if $AnimatedSprite.flip_h else 1
+        bi.set_collision_mask_bit(1, false)
+        get_parent().add_child(bi)
+        print(bi.direction_scale)
